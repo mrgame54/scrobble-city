fc2da463be2c8c7231de03f352911ab8f26d57d4
Axark7 <tjark.strobeck@proton.me>
2026-08-27 00:58:15 +0200

    info API fetch
    it is now possible to fetch a given users information

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const tagsRouter = require('./tags')
 const artistsRouter = require('./artists')
 const tracksRouter = require('./tracks')
 const app = express()
+const API_KEY = process.env.LASTFM_API_KEY
 
 const allowedOrigins = [
     'http://localhost:5173',
@@ -33,16 +34,50 @@ app.use('/tags', tagsRouter)
 app.use('/artists', artistsRouter)
 app.use('/tracks', tracksRouter)
 
+async function fetchUserInfo(username) {
+    console.log('Fetching information for ' + username)
+
+    try {
+        const url = 'http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=' + username + '&api_key=' + API_KEY + '&format=json'
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('HTTP error: ' + response.status)
+        }
+
+        const data = await response.json()
+
+        if (data.error) {
+            throw new Error('Last.fm API Error: ' + data.message)
+        }
+
+        console.log('Successfully fetched all information)')
+
+        return [data.user]
+
+    } catch (error) {
+        console.error('Failed to fetch info: ', error)
+        return []
+    }
+}
+
 //main page
 app.get('/', (req, res) => {
     res.send('Welcome to Scrobble City')
 })
 
 //get username
-app.post('/user', (req, res) => {
-    const username = req.body.username
+app.post('/user', async (req, res) => {
+    const username = req.body.username 
     req.session.username = username
-    res.send('Welcome!')
+
+    try {
+        const fetchedInfo = await fetchUserInfo(username)
+        res.json(fetchedInfo)
+    } catch (error) {
+        console.error('Failed to fetch from Last.fm: ' + error)
+        res.status(500).json({ error: 'Failed to fetch user' })
+    }
 })
 
 //start server
